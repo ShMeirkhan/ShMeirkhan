8e913a1f352f74d91eb6d843da129111d4fbc162
ShMeirkhan <m_shakirbayev@kbtu.kz>
2025-02-20 14:16:51 +0500

    Create q1.py

---
+++ lab3/q1.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(name, age, job):
+        self.name = name
+        self.age = age
+        self.job = job
+        def age(age):
+            self.age = age
+            
